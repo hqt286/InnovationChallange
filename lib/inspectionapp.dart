8995c45c0aa697c88710ddff1f582d7820e8f03d
Hung Tran <hqt286@vt.edu>
2021-02-14 04:27:56 -0500

    Complete Login Page

---
+++ lib/inspectionapp.dart
@@ -0,0 +1,198 @@
+
+import 'dart:ui';
+import 'dart:math';
+import 'package:inspection_app/commentpage.dart';
+import 'package:inspection_app/dto/photodto.dart';
+import 'package:inspection_app/gallerypage.dart';
+import 'package:inspection_app/dto/curbappealdto.dart';
+import 'package:inspection_app/customratingwidget.dart';
+import 'package:inspection_app/customcommentwidget.dart';
+import 'package:inspection_app/dto/userdto.dart';
+import 'package:inspection_app/dto/commentdto.dart';
+import 'package:inspection_app/customdropdownwidget.dart';
+import 'package:inspection_app/customgallerywidget.dart';
+import 'package:inspection_app/customdialogwidget.dart';
+import 'package:flutter/material.dart';
+
+CurbAppealDTO createTestCurbData() {
+
+  CurbAppealDTO curbAppealDTO;
+
+  UserDTO user = UserDTO("Hung Tran");
+  user.email = "hungtran@123.com";
+  user.photoId = NetworkImage("https://picsum.photos/id/237/200");
+
+  curbAppealDTO = new CurbAppealDTO(user);
+  String dummyText = "If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough,"
+      +" the next logical step is to find a random paragraph." +
+      " We created the Random Paragraph Generator with you in mind. The process is quite simple. Choose the number of random paragraphs"
+      +" you'd like to see and click the button. Your chosen number of paragraphs will instantly appear."
+      + "While it may not be obvious to eve";
+
+  curbAppealDTO.comment.content = dummyText;
+
+  CommentDTO comment = new CommentDTO(user);
+  comment.content = dummyText;
+
+  CommentDTO comment1 = new CommentDTO(user);
+  comment1.content = "Dummy";
+
+  curbAppealDTO.photos.add(new PhotoDTO(1, NetworkImage("https://picsum.photos/id/${Random().nextInt(100)}/500"), comment, false));
+  curbAppealDTO.photos.add(new PhotoDTO(2, NetworkImage("https://picsum.photos/id/${Random().nextInt(100)}/500"), comment1, false));
+  curbAppealDTO.photos.add(new PhotoDTO(3, NetworkImage("https://picsum.photos/id/${Random().nextInt(100)}/500"), comment1, false));
+  curbAppealDTO.photos.add(new PhotoDTO(4, NetworkImage("https://picsum.photos/id/${Random().nextInt(100)}/500"), comment1, false));
+  curbAppealDTO.photos.add(new PhotoDTO(5, NetworkImage("https://picsum.photos/id/${Random().nextInt(100)}/500"), comment1, false));
+  curbAppealDTO.photos.add(new PhotoDTO(6, NetworkImage("https://picsum.photos/id/${Random().nextInt(100)}/500"), comment1, false));
+  curbAppealDTO.photos.add(new PhotoDTO(7, NetworkImage("https://picsum.photos/id/${Random().nextInt(100)}/500"), comment1, false));
+
+  return curbAppealDTO;
+}
+
+class InspectionApp extends StatefulWidget{
+
+  CurbAppealDTO _curbAppeal;
+
+  InspectionApp() {
+    _curbAppeal = createTestCurbData();
+  }
+
+  CurbAppealDTO get curbAppeal => _curbAppeal;
+
+  set curbAppeal(CurbAppealDTO value) {
+    _curbAppeal = value;
+  }
+
+  @override
+  State<StatefulWidget> createState() => InspectionAppState();
+}
+
+class InspectionAppState extends State<InspectionApp> {
+
+  void validatedButtonPressed() {
+    if (widget._curbAppeal.comment.content.isEmpty) {
+      widget._curbAppeal.passValidation = false;
+      showDialog(context: context, builder:(BuildContext context) =>
+          CustomDialogWidget("Opsss!", "You forgot to drop some comments", "Okay"));
+    }
+    else {
+      widget._curbAppeal.passValidation = true;
+    }
+    setState(() {
+
+    });
+  }
+
+  void galleryButtonPressed() async{
+    await Navigator.push(context, MaterialPageRoute(builder: (context) => Gallery(widget._curbAppeal.photos)));
+    setState(() {
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(50),
+            child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Ink (
+                        child: IconButton(
+                          icon: Icon(Icons.menu, size: 25.0,),
+                          color: Colors.black,
+                          onPressed: () {
+                            // Navigator.of(context).pop();
+                          },
+                        ),
+                      ),
+
+                      Text("Curb Appeal",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      Ink (
+                        child: IconButton(
+                          icon: Icon(IconData(61707, fontFamily: "MaterialIcons"), size: 25.0,),
+                          color: widget._curbAppeal.passValidation ? Colors.greenAccent.withOpacity(0.7) : Colors.black,
+                          onPressed: validatedButtonPressed,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+            ),
+          ),
+
+          body: SingleChildScrollView (
+            child: Column(
+              children: [
+                Container(
+                  child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 16.0),
+                      child: Text("Comparison to Neighborhood; First Impression / Appearance",
+                        style: TextStyle(fontFamily: 'RobotoMono', fontSize: 16, color: Colors.grey),
+                        textAlign: TextAlign.center,)
+                  ),
+                ),
+
+
+                SizedBox(
+                  height: 100,
+                ),
+
+                //******************************************** Rating Section **************************************************************
+                CustomRatingWidget(widget._curbAppeal),
+                SizedBox(
+                  height: 100,
+                ),
+
+                //******************************************** Trend Section **************************************************************
+
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                  child: CustomDropDown(widget._curbAppeal.trend),
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+
+                //******************************************** Comment Section **************************************************************
+                Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: InkWell(
+                      child: CustomCommentWidget(widget._curbAppeal.comment),
+                      onTap: (){
+                        FocusScopeNode currentFocus = FocusScope.of(context);
+                        if (!currentFocus.hasPrimaryFocus) {
+                          currentFocus.unfocus();
+                        }
+                      },
+                    )
+                ),
+
+                SizedBox(
+
+                  height: 100,
+                ),
+
+                //******************************************** Gallery Section **************************************************************
+                InkWell(
+                  onTap: galleryButtonPressed,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child:  CustomGalleryWidget(widget._curbAppeal.photos),
+                  ),
+                )
+
+              ],
+            ),
+          )
+      ),
+    );
+  }
+}
---
+++ lib/loginpage.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:inspection_app/main.dart';
+import 'package:inspection_app/inspectionapp.dart';
+import 'package:inspection_app/customdialogwidget.dart';
+
+class LoginPage extends StatefulWidget {
+
+  String _userName;
+  String _pwd;
+  bool _loginSuccessful;
+
+
+  LoginPage() {
+    _userName = "";
+    _pwd = "";
+    _loginSuccessful = false;
+  }
+  @override
+  createState() => LoginPageState();
+}
+
+
+class LoginPageState extends State<LoginPage>{
+
+  final _formKey = GlobalKey<FormState>();
+  final _formPwd = GlobalKey<FormState>();
+  TextEditingController _userNameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+
+      // floatingActionButton: FloatingActionButton(
+      //   backgroundColor: Colors.white,
+      //   child: Icon(Icons.done, color: Colors.black,),
+      //   onPressed: () {
+      //     _formKey.currentState.validate();
+      //     _formPwd.currentState.validate();
+      //   },
+      // ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Center(
+              child: Container(
+                width: 300,
+                height: 100,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage("https://www.handhousing.org/wp-content/uploads/Fannie-Mae-Logo.png"),
+                    fit: BoxFit.cover,
+                  ),
+                  border: Border.all(
+                    color: Colors.white,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.only(top: 20, left: 16, right: 16),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _userNameController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "Username cannot be empty";
+                        }
+                        else if (value.length < 3) {
+                          return "Username must be at least 3 characters long";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          labelText: "Username",
+                          labelStyle: TextStyle(color: Color.fromARGB(250, 0, 71, 119)),
+                          hintText: "This is your username",
+                          hintStyle: TextStyle(color: Color.fromARGB(100, 0, 71, 119)),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          )
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+
+            Container(
+              padding: EdgeInsets.only(top: 20, left: 16, right: 16),
+              child: Form(
+                key: _formPwd,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      obscureText: true,
+                      controller: _passwordController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "Password cannot be empty";
+                        }
+                        else if (value.length < 3) {
+                          return "Password must be at least 3 characters long";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          labelStyle: TextStyle(color: Color.fromARGB(250, 0, 71, 119)),
+                          hintText: "This is your password",
+                          hintStyle: TextStyle(color: Color.fromARGB(100, 0, 71, 119)),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          )
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+
+            Container(
+              padding: EdgeInsets.only(top: 70, left: 16, right: 16),
+              child: Form(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    // background color
+                    primary: Color.fromARGB(250, 0, 71, 119),
+                    padding: EdgeInsets.symmetric(horizontal: 160, vertical: 15),
+                    textStyle: TextStyle(fontSize: 20, color: Colors.blueAccent),
+                  ),
+                  child: Text('Log In',
+                  style: TextStyle(color: Colors.white),),
+                  onPressed: () {
+                    if (_userNameController.text == "admin" && _passwordController.text == "admin") {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => InspectionApp()));
+                    }
+                    else {
+                      showDialog(context: context, builder:(BuildContext context) =>
+                          CustomDialogWidget("Opsss!", "Your username and password are not correct", "Okay"));
+                    }
+                  },
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
